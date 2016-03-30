4e924a401a33b48e0cb30b9dfd7e856b5bf61a2b
Paul Cothenet <paul@madkudu.com>
2016-03-30 10:41:18 -0700

    Initial scaffolding

---
+++ index.js
@@ -0,0 +1,7 @@
+(function () {
+
+	exports.encode_url = require('./lib/encode_url');
+
+	exports.decode_url = require('./lib/decode_url');
+
+})();
---
+++ test/encode_url.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('encode_url', function () {
+	it('should bla', function () {
+
+	});
+});
